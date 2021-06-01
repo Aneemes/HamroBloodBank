b6b07a1fab1fe0283adb2d7353361576b2f5791b
riyaranjit <riyaranjit00@gmail.com>
2021-06-01 21:24:16 +0545

    till navbar output

--- BloodBank/views.py
+++ BloodBank/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+
+
+import os
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    return render(request,'BloodBank/home.html')
